e9a9c073baef3a07cb045c8557257139c906ebc0
aaljaseem <anas.aljaseem@gmail.com>
2019-12-27 13:21:07 +0100

    verbetering toolbar

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,6 +9,6 @@ export class UserService {
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+        return this.http.get<User[]>(`http://localhost:3000/ad`);
     }
 }
