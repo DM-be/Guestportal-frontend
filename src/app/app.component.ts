74e31e49df662bd55d9dab749b3a3cd12a9de68e
aaljaseem <anas.aljaseem@gmail.com>
2019-12-27 13:33:39 +0100

    logout zichtbaar maken en functioneel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,8 +20,5 @@ export class AppComponent {
       this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
 
-  logout() {
-      this.authenticationService.logout();
-      this.router.navigate(['/login']);
-  }
+  
 }
--- src/app/top-toolbar/top-toolbar.component.ts
+++ src/app/top-toolbar/top-toolbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  AuthenticationService} from '../services';
+import { User } from '../models/user';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-top-toolbar',
   templateUrl: './top-toolbar.component.html',
   styleUrls: ['./top-toolbar.component.css']
 })
 export class TopToolbarComponent implements OnInit {
-
-  constructor() { }
+  currentUser: User;
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
